ab799225d32dc95759880c9b35719d233ccf62ce
Igor Esteves de Oliveira <igoreoliveira@gmail.com>
2025-08-22 23:15:48 -0300

    Muda o caminho para o mapa interativo

--- src/components/MapEmbeded.tsx
+++ src/components/MapEmbeded.tsx
@@ -4,7 +4,7 @@ export default function MapEmbed() {
   return (
     <div className="w-full mt-6">
       <iframe
-        src="https://labbemviver.github.io/mapa-digital-interativo-bem-viver/mapa"
+        src="https://labbemviver.github.io/mapa-digital-interativo-bem-viver/"
         className="w-full h-[800px] rounded-lg shadow"
         title="Mapa Digital do Bem Viver"
         frameBorder="0"
